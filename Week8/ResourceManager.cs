6851bc0e1562229a89e6b0ea7876b9ee4c2e0303
_KWAZARD_ <144619821+KWAZARD@users.noreply.github.com>
2026-03-28 17:01:09 +0200

    Stage2-completed

---
+++ Week8/ResourceManager.cs
@@ -0,0 +1,32 @@
+﻿namespace Week8;
+
+public class ResourceManager<T> where T : Resource
+{
+    private List<T> manager = new List<T>();
+    public void Add(T resource)
+    {
+        manager.Add(resource);
+        Console.WriteLine("Resource is added to list");
+    }
+
+    public void OpenAll()
+    {
+        foreach (var obj in manager)
+        {
+            obj.Open();
+            
+        }
+
+        Console.WriteLine("All resources is opened");
+    }
+    public void CloseAll()
+    {
+        foreach (var obj in manager)
+        {
+            obj.Close();
+            
+        }
+
+        Console.WriteLine("All resources is closed");
+    }
+}
