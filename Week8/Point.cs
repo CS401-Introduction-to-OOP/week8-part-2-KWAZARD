5fb6fbfb40d0f7666d5ad3c0789ab5e952786ea6
_KWAZARD_ <144619821+KWAZARD@users.noreply.github.com>
2026-03-28 17:21:00 +0200

    Stage1-done

---
+++ Week8/Point.cs
@@ -0,0 +1,17 @@
+﻿namespace Week8;
+
+public struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"({X}, {Y})");
+    }
+}
