9744abc578c659184f61976220e4090abd7b7677
_KWAZARD_ <144619821+KWAZARD@users.noreply.github.com>
2026-03-28 17:58:04 +0200

    stage2-done

---
+++ Week8/DataContainer.cs
@@ -0,0 +1,24 @@
+﻿namespace Week8;
+using System.Collections.Generic;
+   
+
+
+public class DataContainer<T> where T : DataItem
+{
+    private readonly List<T> items = new();
+    public void AddItem(T item)
+    {
+        items.Add(item);
+    }
+    public long GetTotalSize()
+    {
+        long sum = 0;
+        foreach (var VARIABLE in items)
+        {
+            sum += VARIABLE.GetSize();
+        }
+
+        return sum;
+    }
+}
+   
